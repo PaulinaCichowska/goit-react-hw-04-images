9fc99c0dfd25b891fddfa099b8489cee581a65dc
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-10-30 18:01:54 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
 
 export class App extends Component {
   state = {
+    data: null,
     page: 1,
   }
 
@@ -17,32 +18,28 @@ export class App extends Component {
   fetchCats = () => {
     const API_KEY = '38312526-d0ea4ba6d4ac142df4a72a2b0'
     const URL = "https://pixabay.com/api/?q=cat&page=1&key=" + API_KEY + "&image_type=photo&orientation=horizontal&per_page=12"
-    fetch(URL)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json()
-      });
+    try {
+      const data = await fetch(URL);
+      this.setState({ data });
+    } catch (error) {
+      this.setState({ error });
+    }
   }
-  getData = () => {
-    fetchCats()
-    .then((cats) => console.log(cats))
-    .catch((error) => console.log(error));
-  }
+}
 
-  render() {
 
-    return (
-      <>
-        <Searchbar></Searchbar>
-        <ImageGallery>
-          <ImageGalleryItem />
-        </ImageGallery>
+render() {
 
-      </>
-    )
+  return (
+    <>
+      <Searchbar></Searchbar>
+      <ImageGallery>
+        <ImageGalleryItem />
+      </ImageGallery>
 
-  }
+    </>
+  )
+
+}
 
 };
