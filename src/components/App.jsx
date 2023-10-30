1904633842a17bf09bd2d4e52475fced05318620
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-10-30 15:00:44 +0100

    delete bug

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,32 @@ import { ImageGalleryItem } from "./ImageGalleryItem/ImageGalleryItem";
 
 
 export class App extends Component {
-
-  getCośtam = () => {
-
+  state = {
+    page: 1,
   }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+
+  fetchCats = () => {
+    const API_KEY = '38312526-d0ea4ba6d4ac142df4a72a2b0'
+    const URL = "https://pixabay.com/api/?q=cat&page=1&key=" + API_KEY + "&image_type=photo&orientation=horizontal&per_page=12"
+    fetch(URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json()
+      });
+  }
+  getData = () => {
+    fetchCats()
+    .then((cats) => console.log(cats))
+    .catch((error) => console.log(error));
+  }
+
   render() {
 
     return (
