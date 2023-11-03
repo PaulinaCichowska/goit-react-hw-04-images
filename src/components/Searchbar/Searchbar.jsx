6971ded29f92b11d67e8406642d79df87f54fd77
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-11-04 00:25:44 +0100

    czy bóg mnie jeszcze kocha

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -2,7 +2,7 @@ import css from "./Searchbar.module.css"
 export const Searchbar = ({ onSubmit, value, onChange, }) => (
     <header className={css.searchbar}>
         <form onSubmit={onSubmit} className={css.form} >
-            <label className={css.label} for="myInput">Search images</label>
+            <label className={css.label} htmlFor="myInput">Search images</label>
             <input
                 id="myInput"
                 name="search"
