76de6c410d95288b10e9d7f5fa122d70273b1e00
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-11-04 23:13:29 +0100

    up

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,7 +6,7 @@ export const Modal = ({ obj }) => (
         {
             obj.map((elem) => {
                 return < div key={elem.id}>
-                    <a href="/#" className={css.lightbox} id={elem.id}>
+                    <a href="#!" className={css.lightbox} id={elem.id}>
                         <img className={css.lightboxImg} src={elem.largeImageURL} alt={elem.tags} ></img></a>
                 </div>
 
