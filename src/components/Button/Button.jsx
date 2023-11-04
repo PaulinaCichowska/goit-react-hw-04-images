c33dabede4353164cfc4af2b21988b77e0b81762
PaulinaCichowska <114925199+PaulinaCichowska@users.noreply.github.com>
2023-11-04 22:08:29 +0000

    Deploying to gh-pages from @ PaulinaCichowska/goit-react-hw-03-image-finder@c68a5cb5165b4082d6b0ae0e9a5f45acc1e4051c 🚀

--- src/components/Button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import css from './Button.module.css'
-export const Button = ({ loadMore, children }) => (
-    <>
-        <button className={css.button} onClick={loadMore}>{children}</button>
-
-    </>
-)
